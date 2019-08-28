e279a07fe691bd62bb3032d206009de0eda1256f
Eggshells <31183838+eggshellsv@users.noreply.github.com>
2019-08-28 19:20:22 -0400

    Update Vortexs_peepee.cs
    doin your mom

--- Items/Vortexs_peepee.cs
+++ Items/Vortexs_peepee.cs
@@ -8,19 +8,19 @@ namespace spritersguildwip.Items
 		public override void SetStaticDefaults() 
 		{
 			// DisplayName.SetDefault("Vortexs_peepee"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			Tooltip.SetDefault("doin your mom doin doin your mom");
 		}
 
 		public override void SetDefaults() 
 		{
-			item.damage = 50;
+			item.damage = 50000;
 			item.melee = true;
 			item.width = 40;
 			item.height = 40;
-			item.useTime = 20;
+			item.useTime = 1;
 			item.useAnimation = 20;
 			item.useStyle = 1;
-			item.knockBack = 6;
+			item.knockBack = 1;
 			item.value = 10000;
 			item.rare = 2;
 			item.UseSound = SoundID.Item1;
