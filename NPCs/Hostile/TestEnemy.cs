5c6db1984488a6be9a9badb36ecd04f8a49924e2
KittyCatKittyCat <elektroplaygames@gmail.com>
2019-08-31 18:17:53 +0200

    Testenemy

---
+++ NPCs/Hostile/TestEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using spritersguildwip.Ability;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace spritersguildwip.NPCs.Hostile
+{
+    class TestEnemy : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Test Enemy");
+        }
+        public override void SetDefaults()
+        {
+            npc.width = 48;
+            npc.height = 48;
+            npc.lifeMax = 69420;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.aiStyle = -1;
+        }
+    }
+}
