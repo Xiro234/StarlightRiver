5a0d43f4f071c34ae8a0dbfd5b0c4165323ad1ad
NotAScalie <bobfishkins23@yahoo.com>
2019-08-31 00:04:01 -0400

    Smashable Ore!

---
+++ Tiles/VitricOre.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using spritersguildwip.Ability;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace spritersguildwip.Tiles
+{
+    public class VitricOre : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileLavaDeath[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 18, 16, 16 };
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            TileObjectData.addTile(Type);
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("");//Map name
+            AddMapEntry(new Color(0, 0, 0), name);
+            dustType = mod.DustType("Wind");
+            disableSmartCursor = true;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref Color drawColor, ref int nextSpecialDrawIndex)
+        {
+
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer)
+        {
+
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new Vector2(i * 16, j * 16), 32, 48, mod.ItemType("Glassore"), Main.rand.Next(4,12));
+        }
+    }
+
+    public class OreCollide : ModPlayer
+    {
+
+        public override void PreUpdate()
+        {
+            AbilityHandler mp = player.GetModPlayer<AbilityHandler>();
+
+            for (int j = (int)(player.position.Y / 16) - 3; j <= (int)(player.position.Y / 16) + 3; j++)
+            {
+                for (int i = (int)(player.position.X / 16) - 3; i <= (int)(player.position.X / 16) + 3; i++)
+                {
+                        if (Main.tile[i, j].type == mod.TileType("VitricOre") && Main.tile[i, j].frameX == 0 && Main.tile[i, j].frameY == 0)
+                    {
+                        for (float f = 0; f <= 1; f += 1/30f)
+                        {
+                            Vector2 lerped = Vector2.Lerp(player.position, player.oldPosition, f);
+                            if (Collision.CheckAABBvAABBCollision(lerped, new Vector2(32, 48), new Vector2(i * 16, j * 16), new Vector2(32, 48)) && mp.dashcd >= 1)
+                            {
+                                f = 2;
+                                WorldGen.KillTile(i, j);                                                                 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
