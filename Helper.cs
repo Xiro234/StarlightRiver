b4a92ec371b9bf13b46d2a4b981b19e2fbb47684
Dual <31146412+Dual-Iron@users.noreply.github.com>
2019-09-01 00:40:27 -0400

    Extension method usage

---
+++ Helper.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace spritersguildwip
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Kills the NPC.
+        /// </summary>
+        /// <param name="npc"></param>
+        public static void Kill(this NPC npc)
+        {
+            bool modNPCDontDie = npc.modNPC != null && !npc.modNPC.CheckDead();
+            if (modNPCDontDie)
+                return;
+
+            npc.life = 0;
+            npc.checkDead();
+            npc.HitEffect();
+            npc.active = false;
+        }
+    }
+}
