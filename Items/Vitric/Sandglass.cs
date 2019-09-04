43523d8b5828eb94e7714199e4b82b7e7c93c703
Vortex <angelvortexgaming@gmail.com>
2019-09-04 02:17:36 -0400

    added basic recipes and reorganized stuff
    try to keep all vitric related stuff in the vitric folder, and vitric recipes in the recipe file there too. follow that trend with all the other content as well

--- Items/Vitric/Sandglass.cs
+++ Items/Vitric/Sandglass.cs
@@ -1,6 +1,6 @@
 using Terraria.ModLoader;
 
-namespace spritersguildwip.Items
+namespace spritersguildwip.Items.Vitric
 {
     public class Sandglass : ModItem
     {
--- Items/Vitric/VitricSword.cs
+++ Items/Vitric/VitricSword.cs
@@ -8,7 +8,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace spritersguildwip.Items.Weapons.Vitric
+namespace spritersguildwip.Items.Vitric
 {
     class VitricSword : ModItem
     {
