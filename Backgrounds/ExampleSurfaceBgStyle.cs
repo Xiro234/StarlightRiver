a9e5f505b2c138f6decaf5572ab4daf9cd4df53c
NotAScalie <bobfishkins23@yahoo.com>
2019-09-04 21:06:16 -0400

    Broken coped examplemod BG

---
+++ Backgrounds/ExampleSurfaceBgStyle.cs
@@ -0,0 +1,54 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace spritersguildwip.Backgrounds
+{
+	public class ExampleSurfaceBgStyle : ModSurfaceBgStyle
+	{
+		public override bool ChooseBgStyle()
+		{
+			return !Main.gameMenu && Main.LocalPlayer.GetModPlayer<BiomeHandler>(mod).ZoneGlass;
+		}
+
+		// Use this to keep far Backgrounds like the mountains.
+		public override void ModifyFarFades(float[] fades, float transitionSpeed)
+		{
+			for (int i = 0; i < fades.Length; i++)
+			{
+				if (i == Slot)
+				{
+					fades[i] += transitionSpeed;
+					if (fades[i] > 1f)
+					{
+						fades[i] = 1f;
+					}
+				}
+				else
+				{
+					fades[i] -= transitionSpeed;
+					if (fades[i] < 0f)
+					{
+						fades[i] = 0f;
+					}
+				}
+			}
+		}
+
+		public override int ChooseFarTexture()
+		{
+			return mod.GetBackgroundSlot("Backgrounds/Glass4");
+		}
+
+		static int SurfaceFrameCounter = 0;
+		static int SurfaceFrame = 0;
+		public override int ChooseMiddleTexture()
+		{	
+	        return mod.GetBackgroundSlot("Backgrounds/Glass1");			
+		}
+
+		public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b)
+		{
+			return mod.GetBackgroundSlot("Backgrounds/Glass0");
+		}
+	}
+}
---
+++ Backgrounds/ExampleUgBgStyle.cs
@@ -0,0 +1,23 @@
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace spritersguildwip.Backgrounds
+{
+	public class ExampleUgBgStyle : ModUgBgStyle
+	{
+		public override bool ChooseBgStyle()
+		{
+			return Main.LocalPlayer.GetModPlayer<BiomeHandler>(mod).ZoneGlass;
+		}
+
+		public override void FillTextureArray(int[] textureSlots)
+		{
+			textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/Glass0");
+			textureSlots[1] = mod.GetBackgroundSlot("Backgrounds/Glass1");
+			textureSlots[2] = mod.GetBackgroundSlot("Backgrounds/Glass2");
+			textureSlots[3] = mod.GetBackgroundSlot("Backgrounds/Glass3");
+            textureSlots[4] = mod.GetBackgroundSlot("Backgrounds/Glass4");
+        }
+	}
+}
