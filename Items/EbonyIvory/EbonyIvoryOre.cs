aa9a2daa42e61ba319926457e87efcfa10ea81cf
NotAScalie <bobfishkins23@yahoo.com>
2019-10-18 23:47:06 -0400

    Food fix

--- Items/EbonyIvory/EbonyIvoryOre.cs
+++ Items/EbonyIvory/EbonyIvoryOre.cs
@@ -1,7 +1,32 @@
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace StarlightRiver.Items.EbonyIvory
 {
     public class OreEbonyItem : QuickTileItem { public OreEbonyItem() : base("Ebony Ore", "Heavy and Impure", ModContent.TileType<Tiles.OreEbony>(), 1) { } }
     public class OreIvoryItem : QuickMaterial { public OreIvoryItem() : base("Ivory Ore", "Light and Pure", 999, 1000, 4) { } }
+    public class BarEbony : QuickMaterial
+    {
+        public BarEbony() : base("Ebony Bar", "Soft and Heavy", 999, 1000, 1) { }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<OreEbonyItem>(), 4);
+            recipe.AddTile(ModContent.TileType<Tiles.Oven>());
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+    public class BarIvory : QuickMaterial
+    {
+        public BarIvory() : base("Ivory Bar", "Hard and Light", 999, 5000, 4) { }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<OreIvoryItem>(), 4);
+            recipe.AddTile(ModContent.TileType<Tiles.OvenAstral>());
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
 }
