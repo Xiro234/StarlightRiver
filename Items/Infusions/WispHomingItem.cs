a86b936ebf7102ebcf08e881addd764c383ffe60
WhiteIce2112 <50000379+WhiteIce2112@users.noreply.github.com>
2019-10-28 19:36:36 -0400

    Wisp homing infusion item

---
+++ Items/Infusions/WispHomingItem.cs
@@ -0,0 +1,49 @@
+﻿using StarlightRiver.Abilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace StarlightRiver.Items.Infusions
+{
+    public class WispHomingItem : InfusionItem
+    {
+        public WispHomingItem() : base(3) { }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Comet Rush");
+            Tooltip.SetDefault("Fae Flame Infusion\nRelease damaging homing bolts that make enemies take more damage");
+        }
+        public override void UpdateEquip(Player player)
+        {
+            AbilityHandler mp = player.GetModPlayer<AbilityHandler>();
+            if (!(mp.wisp is WispHoming) && !(mp.wisp is WispCombo))
+            {
+                if (mp.wisp is WispWIP)
+                {
+                    mp.wisp = new WispCombo(player);
+                    mp.wisp.Locked = false;
+                }
+                else
+                {
+                    mp.wisp = new WispHoming(player);
+                    mp.wisp.Locked = false;
+                }
+            }
+        }
+
+        public override bool CanEquipAccessory(Player player, int slot)
+        {
+            AbilityHandler mp = player.GetModPlayer<AbilityHandler>();
+            return !mp.wisp.Locked;
+        }
+
+        public override void Unequip(Player player)
+        {
+            player.GetModPlayer<AbilityHandler>().wisp = new Abilities.Wisp(player);
+            player.GetModPlayer<AbilityHandler>().wisp.Locked = false;
+        }
+    }
+}
