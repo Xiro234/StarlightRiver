5eef616f6a167e5978677cd24a47be5b61896267
NotAScalie <bobfishkins23@yahoo.com>
2019-10-28 18:56:02 -0400

    Rudimentary Dolomite stuff

---
+++ Items/Dolomite/DolomiteTiles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Items.Dolomite
+{
+    public class DolomiteItem : QuickTileItem { public DolomiteItem() : base("Dolomite", "", ModContent.TileType<Tiles.Dolomite>(), 0) { } }
+}
