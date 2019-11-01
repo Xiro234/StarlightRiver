e39f2762149f5193dfab3fb5d35b97b172bac305
WhiteIce2112 <50000379+WhiteIce2112@users.noreply.github.com>
2019-10-31 21:25:28 -0400

    VineWhip item created

---
+++ Items/Overgrow/VineWhip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using StarlightRiver.Projectiles.WeaponProjectiles;
+
+namespace StarlightRiver.Items.Overgrow
+{
+    class VineWhip : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Vine Whip");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 40;
+            item.damage = 20;
+            item.melee = true;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.rare = 4;
+            item.useStyle = 5;
+            item.useAnimation = 1;
+            item.useTime = 1;
+            item.channel = true;
+            item.shoot = ModContent.ProjectileType<WhipSegment1>(); 
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int proj = Projectile.NewProjectile(player.Center.X, player.Center.Y, speedX, speedY, ModContent.ProjectileType<WhipSegment1>(), damage, 0, Main.myPlayer);
+            WhipSegment1 whip = Main.projectile[proj].modProjectile as WhipSegment1;
+            Main.projectile[proj].rotation = (Main.MouseWorld - player.Center).ToRotation();
+            Main.projectile[proj].netUpdate = true;
+            return true;
+        }
+    }
+}
