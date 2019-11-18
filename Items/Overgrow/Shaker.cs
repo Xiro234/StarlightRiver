93ddef6afa1aea1cbc773036640d13fc5c235f97
NotAScalie <bobfishkins23@yahoo.com>
2019-11-18 18:11:54 -0500

    THE SHAKER

---
+++ Items/Overgrow/Shaker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace StarlightRiver.Items.Overgrow
+{
+    class Shaker : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Shaker");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 10;
+            item.melee = true;
+            item.width = 40;
+            item.height = 20;
+            item.useTime = 60;
+            item.useAnimation = 1;
+            item.useStyle = 5;
+            item.noMelee = true;
+            item.knockBack = 4;
+            item.rare = 3;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return !Main.projectile.Any(proj => proj.type == ModContent.ProjectileType<Projectiles.WeaponProjectiles.ShakerBall>() && proj.active && Main.player[proj.owner] == player);
+        }
+
+        public override bool UseItem(Player player)
+        {
+            int proj = Projectile.NewProjectile(player.position + new Vector2(0, -30), Vector2.Zero, ModContent.ProjectileType<Projectiles.WeaponProjectiles.ShakerBall>(), item.damage, item.knockBack);
+            player.channel = true;
+            Main.projectile[proj].owner = player.whoAmI;
+            return true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player.channel)
+            {
+                player.velocity.X *= 0.9f;
+                player.GetModPlayer<AnimationHandler>().Lifting = true;
+            }
+        }
+    }
+
+    public class AnimationHandler : ModPlayer
+    {
+        public bool Lifting = false;
+        public override void PostUpdate()
+        {
+            if (Lifting)
+            {
+                player.bodyFrame = new Rectangle(0, 56 * 5, 40, 56);
+            }
+        }
+
+        public override void ResetEffects()
+        {
+            Lifting = false;
+        }
+    }
+}
---
+++ Projectiles/WeaponProjectiles/ShakerBall.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace StarlightRiver.Projectiles.WeaponProjectiles
+{
+    class ShakerBall : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.friendly = false;
+            projectile.width = 64;
+            projectile.height = 64;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 2;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shaker");
+        }
+
+        public override void AI()
+        {
+            Main.NewText("charge:" + projectile.ai[0] + "/ phase:" + projectile.ai[1]);
+            projectile.timeLeft = 2;
+            projectile.scale = projectile.ai[0] < 10 ? (projectile.ai[0] / 10f) : 1;
+
+            Player player = Main.player[projectile.owner];
+
+            if(projectile.ai[0] == 100)
+            {
+                Dust.NewDustPerfect(projectile.Center, ModContent.DustType<Dusts.Gold2>(), Vector2.One.RotatedByRandom(6.28f));
+            }
+
+            if(projectile.ai[1] == 0)
+            {
+                projectile.position = player.position + new Vector2(-15, -60);
+            }
+
+            if (projectile.ai[1] == 0 && projectile.ai[0] < 100)
+            {
+                projectile.ai[0]++;
+                if(projectile.ai[0] == 100)
+                {
+                    for(int k = 0; k <= 100; k++)
+                    {
+                        Dust.NewDustPerfect(projectile.Center, ModContent.DustType<Dusts.Gold2>(), Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(2), 0, default, 1.5f);                       
+                    }
+                    Main.PlaySound(SoundID.NPCDeath7, projectile.Center);
+                }
+                projectile.velocity = Vector2.Zero;
+                float rot = Main.rand.NextFloat(6.28f);
+                Dust.NewDustPerfect(projectile.Center + Vector2.One.RotatedBy(rot) * 35, ModContent.DustType<Dusts.Gold2>(), -Vector2.One.RotatedBy(rot) * 1.5f, 0, default, projectile.ai[0]/100f);
+            }
+
+            if (!player.channel && projectile.ai[1] == 0)
+            {
+                projectile.velocity = Vector2.Normalize(Main.MouseWorld - player.Center) * projectile.ai[0] * 0.1f;
+                projectile.tileCollide = true;
+                projectile.friendly = true;
+                projectile.ai[1] = 1;
+            }
+
+            if (projectile.ai[1] == 1)
+            {
+                projectile.velocity.Y += 0.4f;
+
+                if (projectile.velocity.Y == 0.4f)
+                {
+                    projectile.velocity *= 0;
+                    projectile.ai[1] = 2;
+
+                    player.GetModPlayer<StarlightPlayer>().Shake += (int)(projectile.ai[0] * 0.2f);
+                    for (int k = 0; k <= 100; k++)
+                    {
+                        Dust.NewDustPerfect(projectile.Center + new Vector2 (0, 32), ModContent.DustType<Dusts.Stone>(), new Vector2(0,1).RotatedByRandom(1) * Main.rand.NextFloat(-1, 1) * projectile.ai[0] / 10f);
+                    }
+                    Main.PlaySound(SoundID.Item70, projectile.Center);
+                    Main.PlaySound(SoundID.NPCHit42, projectile.Center);
+                }
+            }
+
+            if(projectile.ai[1] == 2)
+            {
+                projectile.tileCollide = false;
+
+                projectile.velocity += -Vector2.Normalize(projectile.Center - player.Center) * 0.1f;
+                if (projectile.velocity.Length() >= 5) projectile.ai[1] = 3;
+
+                if (Vector2.Distance(projectile.Center, player.Center) <= 20) projectile.timeLeft = 0;
+            }
+
+            if(projectile.ai[1] == 3)
+            {
+                projectile.velocity = -Vector2.Normalize(projectile.Center - player.Center) * 5;
+
+                if (Vector2.Distance(projectile.Center, player.Center) <= 20) projectile.timeLeft = 0;
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            if(projectile.ai[1] != 0)
+            {
+                Player player = Main.player[projectile.owner];
+                for (float k = 0; k <= 1; k += 1 / (Vector2.Distance(player.Center, projectile.Center) / 16))
+                {
+                    spriteBatch.Draw(ModContent.GetTexture("StarlightRiver/Projectiles/WeaponProjectiles/ShakerChain"), Vector2.Lerp(projectile.Center, player.Center, k) - Main.screenPosition, 
+                        new Rectangle(0, 0, 8, 16), lightColor, (projectile.Center - player.Center).ToRotation() + 1.58f, new Vector2(4, 8), 1, 0, 0);
+                }
+            }
+            return true;
+        }
+
+        public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            if (projectile.ai[1] == 0)
+            {
+                float colormult = projectile.ai[0] / 100f * 0.7f;
+                float scale = 1.2f - projectile.ai[0] / 100f * 0.5f;
+                Texture2D tex = ModContent.GetTexture("StarlightRiver/Tiles/Interactive/WispSwitchGlow2");
+                spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, tex.Frame(), Color.LightYellow * colormult, 0, tex.Size() / 2, scale, 0, 0);
+            }
+
+            if (projectile.ai[0] == 100)
+            {
+                Texture2D tex = ModContent.GetTexture("StarlightRiver/Tiles/Interactive/WispSwitchGlow2");
+                spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, tex.Frame(), Color.LightYellow * (6.28f - LegendWorld.rottime) * 0.2f, 0, tex.Size() / 2,LegendWorld.rottime * 0.17f, 0, 0);
+                spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, tex.Frame(), Color.LightYellow * (6.28f - (LegendWorld.rottime + 3.14f)) * 0.2f, 0, tex.Size() / 2, (LegendWorld.rottime + 3.14f) * 0.17f, 0, 0);
+                spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, tex.Frame(), Color.LightYellow * (6.28f - (LegendWorld.rottime - 3.14f)) * 0.2f, 0, tex.Size() / 2, (LegendWorld.rottime - 3.14f) * 0.17f, 0, 0);
+            }
+        }
+    }
+}
