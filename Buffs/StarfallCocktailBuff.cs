ea4f325468f6f676dab086694176f6424a19ae50
KittyCatKittyCat <elektroplaygames@gmail.com>
2020-04-25 02:04:10 +0200

    Stuff

---
+++ Buffs/StarfallCocktailBuff.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Buffs
+{
+    public class StarfallCocktailBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Starfall Cocktail");
+            Description.SetDefault("Stars are falling much more frequently");
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (!Main.dayTime)
+            {
+                if (Main.rand.Next(1000) < 10f * (Main.maxTilesX / 4200))
+                {
+                    int xPos = (Main.rand.Next(Main.maxTilesX - 50) + 100) * 16;
+                    int yPos = Main.rand.Next((int)(Main.maxTilesY * 0.05)) * 16;
+                    Vector2 vector = new Vector2(xPos, yPos);
+                    float speedX = Main.rand.Next(-100, 101);
+                    float speedY = (Main.rand.Next(200) + 100);
+                    float multiplier = (float)Math.Sqrt((speedX * speedX + speedY * speedY));
+                    multiplier = 12 / multiplier;
+                    speedX *= multiplier;
+                    speedY *= multiplier;
+                    Projectile.NewProjectile(vector.X, vector.Y, speedX, speedY, 12, 1000, 10f, player.whoAmI);
+                }
+            }
+        }
+    }
+}
---
+++ Items/Herbology/StarfallCocktail.cs
@@ -0,0 +1,33 @@
+﻿using StarlightRiver.Buffs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Items.Herbology
+{
+    public class StarfallCocktail : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 2;
+            item.useAnimation = 28;
+            item.useTime = 28;
+            item.maxStack = 999;
+            item.width = 20;
+            item.height = 32;
+            item.rare = 4;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.noMelee = true;
+            item.autoReuse = false;
+            item.UseSound = SoundID.Item3;
+            item.consumable = true;
+            item.buffTime = 36000;
+            item.buffType = ModContent.BuffType<StarfallCocktailBuff>();
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Starfall COCKtail");
+            Tooltip.SetDefault("Get high on star juice");
+        }
+    }
+}
