633a5ea11799c3360413320f8e6441e7771151a9
Dual <31146412+Dual-Iron@users.noreply.github.com>
2020-05-23 01:41:25 -0400

    Integrating NetEasy into SLR (init)

---
+++ Packets/UseAbility.cs
@@ -0,0 +1,50 @@
+﻿using NetEasy;
+using StarlightRiver.Abilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace StarlightRiver.Packets
+{
+    [Serializable]
+    public class UseAbility : Module
+    {
+        public UseAbility() { }
+        
+        public UseAbility(int fromWho, Ability ability)
+        {
+            this.fromWho = fromWho;
+            (abActive, abTimer) = (ability.Active, ability.Timer);
+            abType = ability.GetType();
+        }
+
+        private readonly int fromWho;
+        private readonly bool abActive;
+        private readonly int abTimer;
+        private readonly Type abType;
+
+        protected override void Receive()
+        {
+            AbilityHandler mp = Main.player[fromWho].GetModPlayer<AbilityHandler>();
+            Ability ab = mp.Abilities.Single(a => a.GetType() == abType);
+
+            ab.OnCast();
+            (ab.Active, ab.Timer) = (abActive, abTimer);
+        }
+
+        protected override bool PreSend(Node? ignoreClient, Node? toClient)
+        {
+            if (abType == null)
+            {
+                throw new ArgumentException("Specify the ability to sync.");
+            }
+            return base.PreSend(ignoreClient, toClient);
+        }
+    }
+}
+
+// boilerplate code that i wrote at 1:30 am
+// goodnight
