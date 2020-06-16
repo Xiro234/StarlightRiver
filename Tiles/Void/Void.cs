62167c9257b84ce79533bf3c9f707e8f922877bf
NotAScalie <bobfishkins23@yahoo.com>
2020-06-16 01:33:40 -0400

    Light

--- Tiles/Void/Void.cs
+++ Tiles/Void/Void.cs
@@ -24,6 +24,7 @@ namespace StarlightRiver.Tiles.Void
 
         public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref Color drawColor, ref int nextSpecialDrawIndex)
         {
+            Lighting.AddLight(new Vector2(i, j) * 16, new Vector3(1, 0.5f, 1.2f) * 0.6f);
             if (Main.tile[i, j].frameX == 0 && Main.tile[i, j].frameY == 0)
             {
                 for (int k = 0; k <= 2; k++)
@@ -44,6 +45,7 @@ namespace StarlightRiver.Tiles.Void
 
         public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref Color drawColor, ref int nextSpecialDrawIndex)
         {
+            Lighting.AddLight(new Vector2(i, j) * 16, new Vector3(1, 0.5f, 1.2f) * 0.6f);
             if (Main.tile[i, j].frameX == 0 && Main.tile[i, j].frameY == 0)
             {
                 for (int k = 0; k <= 3; k++)
