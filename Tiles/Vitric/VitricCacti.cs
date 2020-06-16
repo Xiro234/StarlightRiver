6664510844d2d838506451211b0a79fd5972a701
bobfishkins23 <bobfishkins23@yahoo.com>
2020-06-16 14:32:39 -0400

    cactus cleaning

--- Tiles/Vitric/VitricCacti.cs
+++ Tiles/Vitric/VitricCacti.cs
@@ -9,6 +9,7 @@ using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
 
 namespace StarlightRiver.Tiles.Vitric
 {
@@ -16,79 +17,27 @@ namespace StarlightRiver.Tiles.Vitric
     {
         public override void SetDefaults()
         {
-            Main.tileLavaDeath[Type] = false;
-            Main.tileFrameImportant[Type] = true;
-
-            TileObjectData.newTile.Width = 2;
-            TileObjectData.newTile.Height = 2;
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
-            TileObjectData.newTile.UsesCustomCanPlace = true;
-            TileObjectData.newTile.CoordinateWidth = 16;
-            TileObjectData.newTile.CoordinatePadding = 2;
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
-            TileObjectData.newTile.Origin = new Point16(0, 0);
-            TileObjectData.newTile.AnchorValidTiles = new int[] { ModContent.TileType<VitricSand>(), ModContent.TileType<VitricSoftSand>() };
+            TileObjectData.newTile.AnchorValidTiles = new int[] { TileType<VitricSand>(), TileType<VitricSoftSand>() };
             TileObjectData.newTile.RandomStyleRange = 4;
             TileObjectData.newTile.StyleHorizontal = true;
-            TileObjectData.addTile(Type);
-
-            ModTranslation name = CreateMapEntryName();
-            name.SetDefault("");
-            AddMapEntry(new Color(80, 131, 142), name);
-            dustType = ModContent.DustType<Dusts.Air>();
-            soundType = SoundID.Shatter;
-            disableSmartCursor = true;
-            minPick = 0;
+            QuickBlock.QuickSetFurniture(this, 2, 2, DustType<Dusts.Glass3>(), SoundID.Dig, false, new Color(80, 131, 142));
         }
 
-        public override void NumDust(int i, int j, bool fail, ref int num)
-        {
-            num = fail ? 1 : 3;
-        }
-
-        public override void KillMultiTile(int i, int j, int frameX, int frameY)
-        {
-            Item.NewItem(new Vector2(i * 16, j * 16), 32, 48, ModContent.ItemType<Items.Vitric.VitricOre>(), Main.rand.Next(2, 5));
-        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), Main.rand.Next(2, 5));
     }
 
     public class VitricSmolCactus : ModTile
     {
         public override void SetDefaults()
         {
-            Main.tileLavaDeath[Type] = false;
-            Main.tileFrameImportant[Type] = true;
-
-            TileObjectData.newTile.Width = 1;
-            TileObjectData.newTile.Height = 1;
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16 };
-            TileObjectData.newTile.UsesCustomCanPlace = true;
-            TileObjectData.newTile.CoordinateWidth = 16;
-            TileObjectData.newTile.CoordinatePadding = 2;
             TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
-            TileObjectData.newTile.Origin = new Point16(0, 0);
-            TileObjectData.newTile.AnchorValidTiles = new int[] { ModContent.TileType<VitricSand>(), ModContent.TileType<VitricSoftSand>() };
+            TileObjectData.newTile.AnchorValidTiles = new int[] { TileType<VitricSand>(), TileType<VitricSoftSand>() };
             TileObjectData.newTile.RandomStyleRange = 4;
             TileObjectData.newTile.StyleHorizontal = true;
-            TileObjectData.addTile(Type);
-
-            ModTranslation name = CreateMapEntryName();
-            name.SetDefault("");
-            AddMapEntry(new Color(80, 131, 142), name);
-            dustType = ModContent.DustType<Dusts.Air>();
-            soundType = SoundID.Shatter;
-            disableSmartCursor = true;
-            minPick = 0;
+            QuickBlock.QuickSetFurniture(this, 1, 1, DustType<Dusts.Glass3>(), SoundID.Dig, false, new Color(80, 131, 142));
         }
 
-        public override void NumDust(int i, int j, bool fail, ref int num)
-        {
-            num = fail ? 1 : 3;
-        }
-
-        public override void KillMultiTile(int i, int j, int frameX, int frameY)
-        {
-            Item.NewItem(new Vector2(i * 16, j * 16), 32, 48, ModContent.ItemType<Items.Vitric.VitricOre>(), 1);
-        }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), 1);
     }
 }
--- Tiles/Vitric/VitricCrystalBig.cs
+++ Tiles/Vitric/VitricCrystalBig.cs
@@ -15,23 +15,13 @@ namespace StarlightRiver.Tiles.Vitric
             return true;
         }
 
-        public override void KillWall(int i, int j, ref bool fail)
-        {
-            fail = true;
-        }
-
-        public override void SetDefaults()
-        {
-            Main.tileSolid[Type] = false;
-            dustType = ModContent.DustType<Dusts.Air>();
-        }
+        public override void KillWall(int i, int j, ref bool fail) => fail = true;
 
         public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
         {
             Texture2D tex = ModContent.GetTexture("StarlightRiver/Tiles/Vitric/CrystalOver1");
 
             spriteBatch.Draw(tex, ((new Vector2(i, j) + Helper.TileAdj) * 16) - Main.screenPosition, tex.Frame(), Color.White, 0, new Vector2(80, 176), 1, 0, 0);
-
         }
     }
 
@@ -42,6 +32,10 @@ namespace StarlightRiver.Tiles.Vitric
             texture = "StarlightRiver/Invisible";
             return true;
         }
-        public override void SetDefaults() { QuickBlock.QuickSet(this, int.MaxValue, ModContent.DustType<Dusts.Air>(), SoundID.CoinPickup, new Color(115, 182, 158), -1); }
+        public override void SetDefaults()
+        {
+            QuickBlock.QuickSet(this, int.MaxValue, ModContent.DustType<Dusts.Air>(), SoundID.CoinPickup, new Color(115, 182, 158), -1);
+            Main.tileBlockLight[Type] = false;
+        }
     }
 }
