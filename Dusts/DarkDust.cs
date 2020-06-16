53c493bd61c4dc85cbcd5f9e01a8df25eb4c7cd6
NotAScalie <bobfishkins23@yahoo.com>
2020-06-16 01:38:49 -0400

    od

--- Dusts/DarkDust.cs
+++ Dusts/DarkDust.cs
@@ -22,17 +22,19 @@ namespace StarlightRiver.Dusts
             dust.color.R--;
             dust.alpha = (int)(dust.fadeIn * 17f / 2f - 17 * (float)(Math.Pow(dust.fadeIn, 2) / 240f));
             dust.position += dust.velocity;
+            dust.rotation += 0.1f;
+            if (dust.fadeIn > 120)
+            {
+                dust.active = false;
+            }
+
             if (dust.velocity.X != 0)
             {
                 dust.fadeIn += 4;
                 dust.velocity *= 0.9f;
                 dust.scale *= 0.9f;
             }
-            dust.rotation += 0.1f;
-            if (dust.fadeIn > 120)
-            {
-                dust.active = false;
-            }
+
             return false;
         }
     }
