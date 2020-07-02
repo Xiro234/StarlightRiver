32bd2bc97b9c3f59aaf775ae6a07afd739123ddc
bobfishkins23 <bobfishkins23@yahoo.com>
2020-07-02 18:11:46 -0400

    Puzzle stuff

---
+++ Tiles/Vitric/Puzzle/Reciever.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarlightRiver.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarlightRiver.Tiles.Vitric.Puzzle
+{
+    class RecieverPuzzle : ModTile
+    {
+        public override void SetDefaults() => QuickBlock.QuickSetFurniture(this, 2, 3, DustType<Dusts.Air>(), SoundID.Tink, false, new Color(0, 255, 255), false, true, "Reciever");
+    }
+
+    class RecieverPlacable : ModTile
+    {
+        public override void SetDefaults() => QuickBlock.QuickSetFurniture(this, 1, 1, DustType<Dusts.Air>(), SoundID.Tink, false, new Color(0, 255, 255), false, true, "Reciever");
+    }
+
+    class RecieverItem : QuickTileItem
+    {
+        public RecieverItem() : base("Light Reciever", "", TileType<RecieverPlacable>(), 0) { }
+    }
+
+    class RecieverItem2 : QuickTileItem
+    {
+        public RecieverItem2() : base("Debug Puzzle Reciever", "", TileType<RecieverPuzzle>(), 0) { }
+    }
+}
