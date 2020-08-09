664cacfb140391ce0fbbbdb7ab4be9e300fde72e
bobfishkins23 <bobfishkins23@yahoo.com>
2020-08-08 22:52:29 -0400

    Windows

--- Tiles/Overgrow/TorchOvergrow.cs
+++ Tiles/Overgrow/TorchOvergrow.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -23,7 +24,7 @@ namespace StarlightRiver.Tiles.Overgrow
 
         public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref Color drawColor, ref int nextSpecialDrawIndex)
         {
-            Dust.NewDustPerfect(new Vector2(4 + i * 16, 2 + j * 16), DustType<Dusts.Gold2>());
+            Dust.NewDustPerfect(new Vector2(4 + i * 16, 2 + j * 16), DustType<Dusts.Gold2>(), Vector2.UnitX * (float)Math.Sin(StarlightWorld.rottime));
             Lighting.AddLight(new Vector2(i * 16, j * 16), new Vector3(255, 200, 110) * 0.004f);
         }
     }
